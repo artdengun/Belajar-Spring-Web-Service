8f59f9dccb8b5319e21fd27c525c8b9f9deecef6
Deni Gunawan <artdengun@gmail.com>
2020-05-10 13:27:11 +0700

    addnewService

--- HELP.md
+++ HELP.md
@@ -1,11 +1,16 @@
+# Read Me First
+The following was discovered as part of building this project:
+
+* The original package name 'com.paytech.Spring-Web-Service' is invalid and this project uses 'com.paytech.SpringWebService' instead.
+
 # Getting Started
 
 ### Reference Documentation
 For further reference, please consider the following sections:
 
 * [Official Apache Maven documentation](https://maven.apache.org/guides/index.html)
-* [Spring Boot Maven Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/2.2.6.RELEASE/maven-plugin/)
-* [Spring Web](https://docs.spring.io/spring-boot/docs/2.2.6.RELEASE/reference/htmlsingle/#boot-features-developing-web-applications)
+* [Spring Boot Maven Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/2.2.7.RELEASE/maven-plugin/)
+* [Spring Web](https://docs.spring.io/spring-boot/docs/2.2.7.RELEASE/reference/htmlsingle/#boot-features-developing-web-applications)
 
 ### Guides
 The following guides illustrate how to use some features concretely:
---
+++ src/main/java/com/paytech/SpringWebService/Controller/WaktuController.java
@@ -0,0 +1,43 @@
+
+package com.paytech.SpringWebService.Controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author denigunawan
+ */
+
+
+@Controller
+public class WaktuController {
+  
+    @GetMapping("/waktu")
+    @ResponseBody
+    public String sekarang(){
+       SimpleDateFormat formatwaktu = new SimpleDateFormat("EEEE, dd MMMM yyyy",new Locale("id","ID"));
+       return formatwaktu.format(new Date());
+    }
+    
+    @GetMapping("/appinfo")
+    @ResponseBody
+    public Map<String, String> info(@RequestParam String nama){
+        Map<String, String> data = new LinkedHashMap<>();
+        
+        data.put("application.name", "Aplikasi SIUP");
+        data.put("application.version","1.0.0");
+        data.put("last.update", "2020-05-10");
+        data.put("author", nama);       
+        return data;
+    }
+}
+
+
